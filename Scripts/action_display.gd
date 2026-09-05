a3e9f24b9e1ad04038c196e7f70354d5e8fca2c4
nicollegordillo <nickygordillo912@gmail.com>
2026-09-05 14:37:43 -0600

    Added: menu sprites

--- Scripts/action_display.gd
+++ Scripts/action_display.gd
@@ -1,14 +1,22 @@
+@tool
 extends Control
 
-@export var button_text: String
+@export var button_text: String:
+	set(value):
+		button_text = value
+		if label:
+			label.text = value
+
+@export var portrait: Texture2D:
+	set(value):
+		portrait = value
+		if texture_rect:
+			texture_rect.texture = value
 
 @onready var label: Label = $Panel2/Label
+@onready var texture_rect: TextureRect = $Panel/TextureRect
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	label.text = button_text
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if portrait:
+		texture_rect.texture = portrait
