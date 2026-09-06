4084570822795a8aa45cfce795927a66bb897885
Jose-Prince <joseprince148@gmail.com>
2026-09-05 18:59:27 -0600

    feat: show char selected

--- Scripts/character_screen.gd
+++ Scripts/character_screen.gd
@@ -1,14 +1,25 @@
 extends Control
 
+@onready var label: Label = $MarginContainer/VBoxContainer/CharSelectorContainer/Player1Container/MarginContainer/Label
+@onready var label_2: Label = $MarginContainer/VBoxContainer/CharSelectorContainer/Player2Container/MarginContainer/Label2
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+
+func _process(_delta: float) -> void:
+	# Jugador 1
+	if Input.is_action_just_pressed("attack_1") \
+	or Input.is_action_just_pressed("block_1") \
+	or Input.is_action_just_pressed("special_1"):
+		label.text = "READY!"
+
+	# Jugador 2
+	if Input.is_action_just_pressed("attack_2") \
+	or Input.is_action_just_pressed("block_2") \
+	or Input.is_action_just_pressed("special_2"):
+		label_2.text = "READY!"
 
 
 func _on_timer_timeout() -> void:
