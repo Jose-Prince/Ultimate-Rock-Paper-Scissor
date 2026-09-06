b938c31376f2b5aa5f30b691249bb7a6e5b7840d
Jose-Prince <joseprince148@gmail.com>
2026-09-05 21:59:33 -0600

    feat: splash screen

---
+++ Scripts/splahs_screen.gd
@@ -0,0 +1,9 @@
+extends VideoStreamPlayer
+
+
+func _ready() -> void:
+	finished.connect(_on_video_finished)
+
+
+func _on_video_finished() -> void:
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
